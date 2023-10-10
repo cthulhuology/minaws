743d1571ed25cf8d014e331b41f9e1e6ff9f173f
David Goehrig <dave@dloh.org>
2023-10-10 10:22:33 +0200

    refactoring

--- minaws.js
+++ minaws.js
@@ -1,106 +1,124 @@
-// minaws.js
+// minaws.js 
 // 
-// minimal interface to aws apis with crpto.subtle
+// minimal interface to aws apis with crpto.subtle 
+// 
+// 
+// MIT License 
+// 
+// Copyright (c) 2023 David J. Goehrig <dave@dloh.org> 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions: 
+// 
+// The above copyright notice and this permission notice shall be included in all 
+// copies or substantial portions of the Software. 
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE. 
 //
-// 
-// MIT License
-// 
-// Copyright (c) 2023 David J. Goehrig <dave@dloh.org>
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-// 
+// aws sigv4 requests using Crypto.Subtle
 
-const yyyymmddthhmmssz = (date) => {
-	return date.toISOString().slice(0, 19).replace(/[^\dT]/g, "") + 'Z'
+// Use STS creds from environment variables
+function credentials() {
+	const accessKeyId = process.env["AWS_ACCESS_KEY_ID"]
+	const secretAccessKey = process.env["AWS_SECRET_ACCESS_KEY"]
+	const sessionToken = process.env["AWS_SESSION_TOKEN"]
+	return { accessKeyId, secretAccessKey, sessionToken }
 }
 
-const yyyymmdd = (date) => {
-	return date.toISOString().replace(/[^\d]/g, "").slice(0,8)
+// YYYYMMDDTHHMMSSZ
+function amznDate(date) {
+	return date.toISOString().slice(0, 19).replace(/[^\dT]/g, "") + 'Z' // YYYYMMDDTHHMMSSZ
 }
 
-const hex(buffer) {
-	const b = typeof(buffer) == 'string' ? s2c(buffer) : buffer
-	return [...b].map( (x) => x.toString(16).padStart(2,"0") ).join("")	
+// YYYMMMDD
+function dateStamp(date) {
+	return date.toISOString().replace(/[^\d]/g, "").slice(0,8)	// YYYYMMDD
 }
 
-const s2c = (s) => {
-	return (typeof(s) == 'Uint8Array') ? s : (new TextEncoder()).encode(s)
+// Turn a string/Uint8Array into a hex-encoded string
+function hex(buffer) {
+	const b = typeof(buffer) == 'string' ? str2chars(buffer) : buffer
+	return [...b].map( (x) => x.toString(16).padStart(2,"0") ).join("")	// left zero pad digits less than 10 base 16
 }
 
-const hash = async (key,data) => {
-	const kb = typeof(key) == 'string' ? s2c(key) : key
-	const db = typeof(data) == 'string' ? s2c(data) : data
+// Convert a string to a Uint8Array
+function str2chars(s) {
+	if (typeof(s) == 'Uint8Array') return s
+	return (new TextEncoder()).encode(s)
+}
+
+// HMAC-SHA-256 hash of key,data
+async function hmac256(key,data) {
+	const kb = typeof(key) == 'string' ? str2chars(key) : key
+	const db = typeof(data) == 'string' ? str2chars(data) : data
 	const k = await crypto.subtle.importKey("raw",kb,{ name: "HMAC", hash: "SHA-256" },false,["sign"])
 	const s = await crypto.subtle.sign({ name: "HMAC", hash: "SHA-256" },k,db)
 	return new Uint8Array(s)
 }
 
-const digest = async (data) => {
-	const db = (typeof(data) == 'string') ? s2c(data) : data
+// SHA-256 digest of the given string 
+async function digest(data) {
+	const db = (typeof(data) == 'string') ? str2chars(data) : data
 	const d = await crypto.subtle.digest("SHA-256",db)	
 	return new Uint8Array(d)
 }
 
-const prefix = new Uint8Array(4 + k.byteLength)
-prefix.set(s2c("AWS4"),0)
-
-const signing = async (secret,date,region,service) => {
-	const ds = yyyymmdd(date)
-	prefix.set(s2c(secret),4)
-	const kdate = await hash(prefix,ds)
-	const kregion = await hash(kdate,region)
-	const kservice = await hash(kregion,service)
-	const ksigning = await hash(kservice,"aws4_request")
-	return ksigning
+// Generate the sigature of the AWS Sigv4 request
+// https://docs.aws.amazon.com/general/latest/gr/create-signed-request.html#calculate-signature
+async function signature(secret,date,region,service,sig) {
+	const ds = dateStamp(date)
+	const k = str2chars(secret)
+	const prefix = new Uint8Array(4 + k.byteLength)
+	prefix.set(str2chars("AWS4"),0)
+	prefix.set(k,4)					// secret access key
+	const s1 = await hmac256(prefix,ds)		// kDate
+	const s2 = await hmac256(s1,region)		// kRegion
+	const s3 = await hmac256(s2,service)		// kService
+	const s4 = await hmac256(s3,"aws4_request")	// kSigning
+	return hex(await hmac256(s4,sig))		// signature
 }
 
-const signature = (signing,data) => {
-	return hex(await hash(signing,data))
-}
-
-const parseUrl = (url) => {
+// Get the host, path, and sorted query params
+function parseUrl(url) {
 	const { protocol, host, pathname, searchParams } = new URL(url)
 	searchParams.sort()
 	const params = searchParams
 	return { protocol, host, pathname, params }
 }
 
-const signable = (key) => {
+// unsignable headers
+function signable(key) {
 	return [ 'authorization', 'content-type', 'content-length', 'user-agent', 'expect', 'x-amzn-trace-id' ].indexOf(key.toLowerCase()) < 0
 }
 
-const copy = (headers,host,session,date) => {
+// Set the amz timestamp, sessiontoken, and host for the request
+function createHeaders(headers,host,session,date) {
+	const h = new Headers(headers)				// copy old
+	h.set("x-amz-date", amznDate(date))			// set date
+	if (session) h.set("x-amz-security-token",session)	// session token
+	h.set("host",host)					// host
+	return h
+}
+
+// Create headers for sigining presigned URLs
+function presignHeaders(headers,host) {
 	const h = new Headers(headers)
-	h.set("x-amz-date", yyyymmddthhmmssz(date))
-	if (session) h.set("x-amz-security-token",session)
 	h.set("host",host)
 	return h
 }
 
-const setHost = (headers,host) => {
-	const h = new Headers(headers)
-	h.set("host",host)
-	return h
-}
-
-const presignQuery = (params,service,region,access,session,date,expires,signedHeaders)  => {
-	params.set("X-Amz-Date", yyyymmddthhmmssz(date))
+function presignQuery(params,service,region,access,session,date,expires,signedHeaders) {
+	params.set("X-Amz-Date", amznDate(date))
 	params.set("X-Amz-Algorithm", "AWS4-HMAC-SHA256")
 	params.set("X-Amz-Credential",access + "/" + scope(service,region,date))
 	if (expires) params.set("X-Amz-Expires", expires)
@@ -109,85 +127,101 @@ const presignQuery = (params,service,region,access,session,date,expires,signedHe
 	return params
 }
 
-const canonical = (headers) => {
+// Generate a sorted eanonical header string, and list of signed headers
+function canonicalize(headers) {
 	const keys = [...headers.keys()].sort()
-	var canonicalHeaders = ""
+	var canonicalHeaders = "" // canonical headers
 	const sa = []
 	for (var k of keys) {
 		canonicalHeaders += k.toLowerCase() + ":" + headers.get(k) + "\n"	
 		if (signable(k)) sa.push(k.toLowerCase())
 	}
-	const signedHeaders = sa.join(";")
+	const signedHeaders = sa.join(";") // signed headers list
 	return {canonicalHeaders, signedHeaders} 
 }
 
-const bodyOf = (request) => {
+// Extract the body of the request or return empty string for digest
+async function bodyOf(request) {
 	return request.body ? new Uint8Array(await request.arrayBuffer()) : null
 }
 
-const requestFrom = (method,path,query,headers,signed,bodysig) => {
-	return  [ method, path, query, headers, signed , bodysig ].join("\n")
+// Generate the canonical request string
+// https://docs.aws.amazon.com/general/latest/gr/create-signed-request.html#create-canonical-request
+function canonicalRequest(method,path,query,headers,signed,bodysig) {
+	return  method + "\n" +
+		path + "\n" +
+		query + "\n" +
+		headers + "\n" +	// NB: each header ends in \n so \n gives us a blank line
+		signed + "\n" + 
+		bodysig
 }
 
-const scope = (service,region,date) => {
-	return [ yyyymmdd(date),region, service,"aws4_request"].join("/")
+// Create the scope string
+function scope(service,region,date) {
+	return  dateStamp(date) + "/" + 
+		region + "/" + 
+		service + "/aws4_request"
 }
 
-const stringToSign = (service,region,date,canonicalsig) => {
-	return [ "AWS4-HMAC-SHA256", yyyymmddthhmmssz(date), scope(service,region,date), canonicalsig ].join("\n")
+// Generate the signing string, with the digest of the canonical request
+// https://docs.aws.amazon.com/general/latest/gr/create-signed-request.html#create-string-to-sign
+function stringToSign(service,region,date,canonicalsig) {
+	return "AWS4-HMAC-SHA256\n" +					// algorithm
+		amznDate(date) + "\n" +					// timestamp
+		scope(service,region,date) + "\n" +			// scope
+		canonicalsig						// canonical request digest
 }
 		    
-const authToken = (access,service,region,date,signedHeaders,sig) => {
-	return  [ "AWS4-HMAC-SHA256 ", "Credential=", access, "/", scope(service,region,date), ", ", "SignedHeaders=" ,signedHeaders , ", " , "Signature=" , sig ].join("')
+// Generate the authorization header for the SigV4 request
+function authToken(access,service,region,date,signedHeaders,sig) {
+	return  "AWS4-HMAC-SHA256 " + 
+		"Credential=" + access + "/" + scope(service,region,date) + ", " +
+		"SignedHeaders=" + signedHeaders + ", " + 
+		"Signature=" + sig
 }
 
-var sig = false
-
-const sign = async (service,region,creds,date,request) => {
+// Sign a request, returns the signed Request object
+// https://docs.aws.amazon.com/general/latest/gr/create-signed-request.html#add-signature-to-request
+async function sign(service,region,creds,date,request) {
 	const access = creds.accessKeyId
 	const secret = creds.secretAccessKey
 	const session = creds.sessionToken
 	const method = request.method
 	const url = request.url
 	const { host, pathname, params } = parseUrl(url)
-	const headers = copy(request.headers,host,session,date)
-	const { canonicalHeaders, signedHeaders } = canonical(headers)
+	const headers = createHeaders(request.headers,host,session,date)
+	const { canonicalHeaders, signedHeaders } = canonicalize(headers)
 	const body = await bodyOf(request)
 	const bodysig = body ? hex(await digest(body )) : hex(await digest(""))
-	const cr = requestFrom(method,pathname,params,canonicalHeaders,signedHeaders,bodysig)
+	const cr = canonicalRequest(method,pathname,params,canonicalHeaders,signedHeaders,bodysig)
 	const crsig = hex(await digest(cr))
-	const s2s = stringToSign(service,region,date,crsig)
-	sig = sig ? sig : await signing(secret,date,region,service,ss)
-	const token = signature(sig,s2s)
-	const auth = authToken(access,service,region,date,signedHeaders,token)
+	const ss = stringToSign(service,region,date,crsig)
+	const sig = await signature(secret,date,region,service,ss)
+	const auth = authToken(access,service,region,date,signedHeaders,sig)
 	headers.set("Authorization",auth)
 	const redirect = request.redirect
 	return body ? new Request(url,{ headers,method,body,redirect }) : new Request(url, { headers, method, redirect })
 }
 
-const presign = async (service,region,creds,date,expires,payload,request) => {
+// Generate a presigned URL
+// https://docs.aws.amazon.com/general/latest/gr/create-signed-request.html#add-signature-to-request
+// https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html
+async function signUrl(service,region,creds,date,expires,payload,request) {
 	const access = creds.accessKeyId
 	const secret = creds.secretAccessKey
 	const session = creds.sessionToken
 	const method = request.method
 	const url = request.url
 	const { protocol, host, pathname, params } = parseUrl(url)
-	const headers = setHost(request.headers,host)
-	const { canonicalHeaders, signedHeaders } = canonical(headers)
+	const headers = presignHeaders(request.headers,host)
+	const { canonicalHeaders, signedHeaders } = canonicalize(headers)
 	const query = presignQuery(params,service,region,access,session,date,expires,signedHeaders)
-	const cr = requestFrom(method,pathname,query,canonicalHeaders,signedHeaders,payload !== null ? hex(await digest(payload)) :"UNSIGNED-PAYLOAD")
+	const cr = canonicalRequest(method,pathname,query,canonicalHeaders,signedHeaders,payload !== null ? hex(await digest(payload)) :"UNSIGNED-PAYLOAD")
 	const crsig = hex(await digest(cr))
-	const s2s = stringToSign(service,region,date,crsig)
-	sig = sig ? sig : await signing(secret,date,region,service)
-	const token = signature(sig,s2s)
-	return [ protocol, "//", host, pathname, "?", params, "&X-Amz-Signature=", token, (session ? "&X-Amz-Security-Token=" + encodeURIComponent(session) : "")].join("")
+	const ss = stringToSign(service,region,date,crsig)
+	const sig = await signature(secret,date,region,service,ss)
+	return protocol + "//" + host + pathname + "?" + params + "&X-Amz-Signature=" + sig +
+		(session ? "&X-Amz-Security-Token=" + encodeURIComponent(session) : "")
 }
 
-const credentials = () => {
-	const accessKeyId = process.env["AWS_ACCESS_KEY_ID"]
-	const secretAccessKey = process.env["AWS_SECRET_ACCESS_KEY"]
-	const sessionToken = process.env["AWS_SESSION_TOKEN"]
-	return { accessKeyId, secretAccessKey, sessionToken }
-}
-
-module.exports = { sign, presign, credentials }
+module.exports = { sign, signUrl, credentials }
